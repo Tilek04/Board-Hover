74c02f66df164fc16c47c098afa6efbcdf6275ca
Tilek04 <tilek.dzenishev@gmail.com>
2021-06-15 16:00:54 +0600

    initial

---
+++ app.js
@@ -0,0 +1,37 @@
+const board = document.querySelector('#board')
+const colors = ['#3974E1', '#23E1D1', '#E1BA2C', '#E10047',
+ '#8300E1', '#006262', '#F07427',]
+const SQUARES_NUMBER = 500
+
+for(let i = 0; i < SQUARES_NUMBER; i++) {
+ const square = document.createElement('div')
+ square.classList.add('square')
+
+square.addEventListener('mouseover', () => 
+    setColor(square))
+   
+
+    square.addEventListener('mouseleave', () => 
+    removeColor(square))
+    
+
+ board.append(square )
+}
+
+function setColor (element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+ 
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor () {
+ const index = Math.floor(Math.random() * colors.length)
+
+ return colors[index]
+} 
